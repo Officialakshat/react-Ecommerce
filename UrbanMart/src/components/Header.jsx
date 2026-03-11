21e555d4e85c56ad650285020282244876d68506
Officialakshat <165244500+Officialakshat@users.noreply.github.com>
2026-03-11 23:33:02 +0530

    update (routing)

--- UrbanMart/src/components/Header.jsx
+++ UrbanMart/src/components/Header.jsx
@@ -2,20 +2,28 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <nav className="h-20 bg-gray-300  text-green-500 flex justify-between items-center px-6 gap-5 ">
-      <div className="flex items-center gap-4">
+    <nav className="min-h-20 bg-gray-100  text-green-400 flex flex-col md:flex-row justify-between items-center px-6 gap-5 ">
+      <div className="flex items-center gap-4 ">
         <img
-          className="h-14 w-14 "
+          className="h-12 w-12 "
           src="https://cdn-icons-png.flaticon.com/512/2662/2662459.png"
           alt=""
         />
-        <p className="text-3xl font-semibold">UrbanMart</p>
+        <p className="text-2xl md:text-3xl font-semibold">UrbanMart</p>
       </div>
-      <div className="space-x-6 text-xl font-semibold">
-        <Link to="/">Home</Link>
-        <Link to="/cart">Cart</Link>
-        <Link to="/checkout">Checkout</Link>
-        <Link to="/productDetails">Product Details</Link>
+      <div className="space-x-6  font-semibold  justify-center items-center">
+        <ul className="gap-4 flex flex-col md:flex-row">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/cart">Cart</Link>
+          </li>
+          <li>
+            <Link to="/checkout">Checkout</Link>
+          </li>
+        </ul>
+        {/* <Link to="/productDetails">Product Details</Link> */}
       </div>
     </nav>
   );
